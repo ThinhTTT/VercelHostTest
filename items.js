ced950d7ea6360a65e8e6c2ad84db8446c223623
ThinhTT <thinhtt1@vitalify.asia>
2025-06-20 15:14:37 +0700

    up

--- items.js
+++ items.js
@@ -1,63 +1,63 @@
 //add item to this file
 const items = {
-    VFATree: {
-      name: "VFATree",
-      size: [1, 1],
-      walkable: false,
-    },
-    udlTableChair: {
-      name: "udlTableChair",
-      size: [4, 4],
-      walkable: true,
-    },
-    udlTableBar: {
-      name: "udlTableBar",
-      size: [12, 1],
-      walkable: false,
-    },
-    wall: {
-      name: "wall",
-      size: [3, 1],
-      wall: true,
-      walkable: false,
-    },
-    udlWaterCooler: {
-      name: "udlWaterCooler",
-      size: [1, 1],
-      walkable: false,
-    },
-    udlSofa: {
-      name: "udlSofa",
-      size: [4, 2],
-      walkable: true,
-    },
-    udlLogo: {
-      name: "udlLogo",
-      size: [2, 1],
-      wall: true,
-      walkable: true,
-    },
-    udlBlind: {
-      name: "udlBlind",
-      size: [4, 1],
-      wall: true,
-      walkable: false,
-    },
-    udlStairs: {
-      name: "udlStairs",
-      size: [2, 6],
-      walkable: false,
-    },
-    udlStaircase: {
-      name: "udlStaircase",
-      size: [5, 6],
-      walkable: false,
-    },
-    tv: {
-      name: "tv",
-      size: [2, 1],
-      walkable: true,
-    },
+    // VFATree: {
+    //   name: "VFATree",
+    //   size: [1, 1],
+    //   walkable: false,
+    // },
+    // udlTableChair: {
+    //   name: "udlTableChair",
+    //   size: [4, 4],
+    //   walkable: true,
+    // },
+    // udlTableBar: {
+    //   name: "udlTableBar",
+    //   size: [12, 1],
+    //   walkable: false,
+    // },
+    // wall: {
+    //   name: "wall",
+    //   size: [3, 1],
+    //   wall: true,
+    //   walkable: false,
+    // },
+    // udlWaterCooler: {
+    //   name: "udlWaterCooler",
+    //   size: [1, 1],
+    //   walkable: false,
+    // },
+    // udlSofa: {
+    //   name: "udlSofa",
+    //   size: [4, 2],
+    //   walkable: true,
+    // },
+    // udlLogo: {
+    //   name: "udlLogo",
+    //   size: [2, 1],
+    //   wall: true,
+    //   walkable: true,
+    // },
+    // udlBlind: {
+    //   name: "udlBlind",
+    //   size: [4, 1],
+    //   wall: true,
+    //   walkable: false,
+    // },
+    // udlStairs: {
+    //   name: "udlStairs",
+    //   size: [2, 6],
+    //   walkable: false,
+    // },
+    // udlStaircase: {
+    //   name: "udlStaircase",
+    //   size: [5, 6],
+    //   walkable: false,
+    // },
+    // tv: {
+    //   name: "tv",
+    //   size: [2, 1],
+    //   walkable: true,
+    // },
   };
   
   export default items;
